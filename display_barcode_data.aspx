e3611ec22e83c369e6d53ce69c6ea7f2bb64dac3
sj1506 <jsaksham57@gmail.com>
2018-01-17 16:55:31 +0530

    changes

--- display_barcode_data.aspx
+++ display_barcode_data.aspx
@@ -5,6 +5,28 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Bar Code Data</title>
+    <link href="stylesheet.css" rel="stylesheet" type="text/css" />
+    <link href="bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script src="bootstrap.min.js" type="text/javascript"></script>
+    <link href="font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="Admin/assets/css/bootstrap.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        function showModal() {
+            $("#myModal").modal('show');
+        }
+        function printdiv() {
+            var divContents = document.getElementById("print").innerHTML;
+            var printWindow = window.open('', '', 'height=200,width=400');
+            printWindow.document.write('<html>');
+            printWindow.document.write('<body >');
+            printWindow.document.write(divContents);
+            printWindow.document.write('</body></html>');
+            printWindow.document.close();
+            printWindow.print();
+        }
+    </script>
 </head>
 <body>
     <form id="form1" runat="server">
@@ -25,9 +47,48 @@
            <ItemTemplate>
             <asp:Label ID="lbl_minimum_quantity" runat="server" Text= '<%# Eval("total_cost")%>'></asp:Label>
            </ItemTemplate>
+           </asp:TemplateField>
+           <asp:TemplateField>
+           <ItemTemplate>
+           <asp:Button ID="btn_print" runat="server" Text="print" onclick="btn_print_Click"/>
+           </ItemTemplate>
            </asp:TemplateField>     
         </Columns>
         </asp:GridView>
+    </div>
+
+       <asp:TextBox ID="txtCode" runat="server"></asp:TextBox>
+                   <asp:Button ID="btnGenerate" runat="server" Text="Generate" onclick="btnGenerate_Click" AutoPostBack="true"/>
+
+              <div id="myModal" class="modal fade" style="margin-left: 1PX; width: 100%;" data-toggle="modal"
+        data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog" style="width: 85%;">
+            <!-- Modal content-->
+            <div class="modal-content" style="height: 700px; overflow-y: scroll;">
+                <div class="modal-body" style="background-color: #2e6da4; height: 50px;">
+                    <button type="button" class="close" data-dismiss="modal" style="background: #2e6da4;
+                        border-radius: 50% 50% 50% 50%; -webkit-border-radius: 50% 50% 50% 50%; font-size: 18px;
+                        font-weight: bold; position: absolute; right: 15px; text-align: center; top: 10px;
+                        width: 30px; height: 30px; line-height: 30px; color: white; opacity: 1;">
+                        &times;
+                    </button>
+                    <div class="lightbox-inner">
+                                            
+                    </div>
+                    <div class="container">
+                           <div id="print">
+                            <asp:HiddenField ID="hdn1" runat="server" />
+                            <asp:HiddenField ID="hdn2" runat="server" />
+                            <asp:HiddenField ID="hdn3" runat="server" />
+                      <asp:PlaceHolder ID="plBarCode" runat="server" />
+                          </div>
+                      
+                </div>
+                    </div>
+                <div class="modal-footer">
+                </div>
+            </div>
+        </div>
     </div>
     </form>
 </body>
--- display_barcode_data.aspx.cs
+++ display_barcode_data.aspx.cs
@@ -12,6 +12,9 @@ using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 using System.Data.SqlClient;
 using System.Web.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 public partial class display_barcode_data : System.Web.UI.Page
 {
     Class1 cl = new Class1();
@@ -26,7 +29,7 @@ public partial class display_barcode_data : System.Web.UI.Page
     {
         string connectionstring = WebConfigurationManager.ConnectionStrings["connection"].ConnectionString;
         SqlConnection cnn = new SqlConnection(connectionstring);
-        string qry = "select a.p_title, a.sub_unit, b.total_cost from tbl_product as a , tbl_transaction_buy_detail as b";
+        string qry = "select a.p_title, a.sub_unit, b.total_cost from tbl_product as a left join tbl_transaction_buy_detail as b on a.p_id=b.p_id";
         SqlCommand cmd = new SqlCommand(qry, cnn);
         SqlDataAdapter ad = new SqlDataAdapter(cmd);
         DataTable dt = new DataTable();
@@ -34,5 +37,58 @@ public partial class display_barcode_data : System.Web.UI.Page
         cnn.Close();
         GridView1.DataSource = dt;
         GridView1.DataBind();
+    }
+    protected void btn_print_Click(object sender, EventArgs e)
+    {
+        string barCode = txtCode.Text;
+        System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+        using (Bitmap bitMap = new Bitmap(barCode.Length * 40, 80))
+        {
+            using (Graphics graphics = Graphics.FromImage(bitMap))
+            {
+                Font oFont = new Font("IDAutomationHC39M", 16);
+                PointF point = new PointF(2f, 2f);
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                SolidBrush whiteBrush = new SolidBrush(Color.White);
+                graphics.FillRectangle(whiteBrush, 0, 0, bitMap.Width, bitMap.Height);
+                graphics.DrawString("*" + barCode + "*", oFont, blackBrush, point);
+            }
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] byteImage = ms.ToArray();
+
+                Convert.ToBase64String(byteImage);
+                imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+            }
+            plBarCode.Controls.Add(imgBarCode);
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "showModal();", true);
+        }
+    }
+    protected void btnGenerate_Click(object sender, EventArgs e)
+    {
+        string barCode = txtCode.Text;
+        System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+        using (Bitmap bitMap = new Bitmap(barCode.Length * 40, 80))
+        {
+            using (Graphics graphics = Graphics.FromImage(bitMap))
+            {
+                Font oFont = new Font("IDAutomationHC39M", 16);
+                PointF point = new PointF(2f, 2f);
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                SolidBrush whiteBrush = new SolidBrush(Color.White);
+                graphics.FillRectangle(whiteBrush, 0, 0, bitMap.Width, bitMap.Height);
+                graphics.DrawString("*" + barCode + "*", oFont, blackBrush, point);
+            }
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] byteImage = ms.ToArray();
+
+                Convert.ToBase64String(byteImage);
+                imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+            }
+            plBarCode.Controls.Add(imgBarCode);
+        }
     }
 }
