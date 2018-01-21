b420c2f35337c7653630866bb3c3b3ae8fd3cd4d
sj1506 <jsaksham57@gmail.com>
2018-01-21 14:48:04 +0530

    add sell return page

---
+++ Sell_return.aspx
@@ -0,0 +1,182 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Sell_return.aspx.cs" Inherits="Sell_return" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Sell Return</title>
+    <link href="bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <script src="bootstrap.min.js" type="text/javascript"></script>
+    <link href="font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <script src="jquery.min.js" type="text/javascript"></script>
+    <style type ="text/css">
+    .set
+    {
+        list-style:none;
+        float: left;
+    }
+    .set li
+    {
+        float: left;
+    }
+    .first
+    {
+        width: 200%;
+    }
+    .second
+    {
+        width: 200%;
+    }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+                   
+                     <div style="height: 50px; width: 100%;     margin-top: 20px;
+    margin-left: 51px;">
+                    <asp:Label ID="lbl_select" runat="server" Text=""><b>Please Select Customer : </b></asp:Label>
+                    <asp:DropDownList ID="ddl_cs" Width="20%" class="drop" runat="server" 
+                        AutoPostBack="true" onselectedindexchanged="ddl_cs_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </div>
+
+
+          <div >
+                            <asp:HiddenField ID="hdn1" runat="server" />
+                            <asp:HiddenField ID="hdn2" runat="server" />
+                        <table id="tblData" >	
+                        <thead>
+                         <tr> 
+                         <th><asp:Label ID="lbl_itemname" runat="server" Text="Item Name" style="
+    margin-left: 52px;"></asp:Label></th>
+                          <th><asp:Label ID="lbl_qty" runat="server" Text="Qty" style="margin-left: 33px;"></asp:Label></th> 
+                           <th><asp:Label ID="lbl_rate" runat="server" Text="rate" style="margin-left: 33px;" ></asp:Label> </th>
+                            <th><asp:Label ID="lbl_unit" runat="server" Text="per" style="margin-left: 33px;"></asp:Label> </th>
+                           <th> <asp:Label ID="lbl_totalcost" runat="server" Text="Total cost" style="margin-left: 33px;"></asp:Label></th>
+                           <th><asp:Label ID="lbl_narration" runat="server" Text="Narration" style="margin-left: 33px;"></asp:Label></th>
+                           <th></th>
+                           </tr>
+                           <tr>
+                               <th>
+                             <asp:DropDownList ID="ddl_product" runat="server" style="     margin-left: 37px;">
+                             </asp:DropDownList>
+                         </th>
+                           <td><asp:TextBox ID="qty" runat="server" style="  margin-left: 15px;   width: 90px;"></asp:TextBox></td>
+                           <td><asp:TextBox ID="rate" runat="server"  style=" margin-left: 15px; width: 90px;"  
+                                   AutoPostBack="true" ontextchanged="rate_TextChanged"></asp:TextBox></td>
+                           <td> 
+                               <asp:DropDownList ID="ddl_unit" runat="server" style=" margin-left: 15px;">
+                               </asp:DropDownList>
+                           </td>
+                  <td><asp:TextBox ID="txt_total_cost" runat="server"  style=" margin-left: 15px; width: 110px;" 
+                                    AutoPostBack="true" ></asp:TextBox></td>
+
+                       <td><asp:TextBox ID="txt_narration" runat="server" style="  margin-left: 15px;   width: 200px;"></asp:TextBox></td>
+                           <td><asp:Button ID="btn_addmore" runat="server" Text="Add More" style=" margin-left: 15px;" 
+                                onclick="btn_addmore_Click" />
+  
+                                </td>
+                           </tr>
+                            </thead> 
+                               </table>
+             <asp:Label ID="lbl_msg" Font-Bold="True" BackColor="#FFFF66" ForeColor="#FF3300"
+                                                    runat="server" Text=""></asp:Label>
+                        </div>
+    
+                                             <%-- list view for transaction_buy detail--%>
+                                                   <div class="row">
+                            <div class="col-lg-12">
+                                <div class="table-responsive">
+    <asp:ListView ID="ListView2" runat="server" 
+          onitemcommand="ListView2_ItemCommand" GroupPlaceholderID="groupPlaceHolder2"
+                                        ItemPlaceholderID="itemPlaceHolder2">
+                         <LayoutTemplate>
+                                            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                                <thead>
+                                                    <tr>
+                                                        <th>
+                                                            S.No :
+                                                        </th>
+                                                        <th>
+                                                             Item Name :
+                                                        </th>
+                                                        <th>
+                                                            QTY :
+                                                        </th>
+                                                        <th>
+                                                            Rate :
+                                                        </th>
+                                                        <th>
+                                                            Per :
+                                                        </th>
+                                                        <th>
+                                                            Total Cost :
+                                                        </th>
+                                                        <td>
+                                                          Customer Name :
+                                                        </td>
+                                                        <td>
+                                                          Narration :
+                                                        </td>
+                                                        <th>
+                                                            Edit :
+                                                        </th>
+                                                        <th>
+                                                            Delete :
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                                <asp:PlaceHolder runat="server" ID="groupPlaceHolder2"></asp:PlaceHolder>
+                                            </table>
+                                        </LayoutTemplate>
+                                        <GroupTemplate>
+                                            <tr>
+                                                <asp:PlaceHolder runat="server" ID="itemPlaceHolder2"></asp:PlaceHolder>
+                                            </tr>
+                                        </GroupTemplate>
+                                        <ItemTemplate>
+                                            <td>
+                                                <%# Container.DataItemIndex+1 %>
+                                            </td>
+                                            <td>
+                                                <%# Eval("item_name")%>
+                                            </td>
+                                            <td>
+                                                <%# Eval("qty")%>
+                                            </td>
+                                            <td>
+                                                <%# Eval("rate")%>
+                                            </td>
+                                            <td>
+                                                <%# Eval("unit")%>
+                                            </td>
+                                            <td>
+                                                <%# Eval("total")%>
+                                            </td>
+                                            <td>
+                                             <%# Eval("customer_name")%>
+                                            </td>
+                                            <td>
+                                             <%# Eval("narration")%>
+                                            </td>
+                                            <td>
+                                                <asp:Button ID="btnedit" runat="server" type="submit" class="btn btn-primary" CommandName="CmdEdit"
+                                                    Text="Edit" CommandArgument='<%# Eval("sr_id")%>' />
+                                            </td>
+                                            <td>
+                                                <asp:Button ID="Button2" runat="server" type="submit" class="btn btn-primary" CommandName="CmdDelete"
+                                                    Text="Delete" CommandArgument='<% #Eval("sr_id") %>' OnClientClick="javascript:return confirm('Are you sure you want to Delete?');" />
+                                            </td>
+                                        </ItemTemplate>
+                                    </asp:ListView>
+                                </div>
+                            </div>
+                                                 
+                                                    </div>
+                                                   <%--end details for list view--%>
+                             <asp:Button ID="btn_final" runat="server" Text="Save" 
+                                  Height="21px" Width="91px" style=" margin-left: 90%;
+                                 margin-top: 20px;" onclick="btn_final_Click"/>
+    </form>
+</body>
+</html>
---
+++ Sell_return.aspx.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+public partial class Sell_return : System.Web.UI.Page
+{
+    String conn = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+    DataSet ds = new DataSet();
+    DataTable dt = new DataTable();
+    Class1 cl = new Class1();
+    string id;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //    BindListView();
+            string qry = "select p_id, p_title from tbl_product";
+            cl.filldropdown(qry, "p_id", "p_title", ddl_product);
+            string qury = "select cs_id, name from tbl_customers";
+            cl.filldropdown(qury, "cs_id", "name", ddl_cs);
+            string q = "select unit_id , unit_name from tbl_unit";
+            cl.filldropdown(q, "unit_id", "unit_name", ddl_unit);
+        }
+    }
+    protected void btn_addmore_Click(object sender, EventArgs e)
+    {
+        if (btn_addmore.Text == "Add More")
+        {
+            submit1();
+            BindListView1();
+            clear1();
+        }
+        if (btn_addmore.Text == "update")
+        {
+            Update1();
+        }
+
+    }
+    private void BindListView1()
+    {
+        try
+        {
+            SqlConnection con = new SqlConnection(conn);
+            if (con.State == 0)
+            {
+                con.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("select * from tbl_temp_sell_return where customer_id='" + ddl_cs.SelectedValue + "'", con);
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            ListView2.DataSource = dt;
+            ListView2.DataBind();
+        }
+        catch
+        {
+        }
+    }
+    public void submit1()
+    {
+        SqlConnection con = new SqlConnection(conn);
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "sp_temp_sell_return";
+        cmd.Parameters.AddWithValue("@action", btn_addmore.Text.ToLower());
+        cmd.Parameters.AddWithValue("@sr_id", hdn1.Value.ToString());
+        cmd.Parameters.AddWithValue("@item_id", ddl_product.SelectedValue.ToString());
+        cmd.Parameters.AddWithValue("@item_name", ddl_product.SelectedItem.ToString());
+        cmd.Parameters.AddWithValue("@qty", qty.Text.ToString());
+        cmd.Parameters.AddWithValue("@rate", rate.Text.ToString());
+        cmd.Parameters.AddWithValue("@unit", ddl_unit.SelectedValue.ToString());
+        cmd.Parameters.AddWithValue("@total", txt_total_cost.Text.ToString());
+        cmd.Parameters.AddWithValue("@customer_id", ddl_cs.SelectedValue.ToString());
+        cmd.Parameters.AddWithValue("@customer_name", ddl_cs.SelectedItem.ToString());
+        cmd.Parameters.AddWithValue("@narration", txt_narration.Text.ToString());
+        cmd.Parameters.Add("@result", SqlDbType.NVarChar, 500);
+        cmd.Parameters["@result"].Direction = ParameterDirection.Output;
+        cmd.Connection = con;
+        con.Open();
+        cmd.ExecuteNonQuery();
+        lbl_msg.Text = cmd.Parameters["@result"].Value.ToString();
+        BindListView1();
+        btn_addmore.Text = "Add More";
+        clear1();
+    }
+    private void clear1()
+    {
+        ddl_product.SelectedIndex = -1;
+        qty.Text = "";
+        rate.Text = "";
+        ddl_unit.SelectedIndex=-1;
+        txt_total_cost.Text = "";
+        ddl_cs.SelectedIndex = 1;
+        txt_narration.Text = "";
+    }
+    public void Update1()
+    {
+        SqlConnection con = new SqlConnection(conn);
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandText = "sp_temp_sell_return";
+        cmd.Parameters.AddWithValue("@action", btn_addmore.Text.ToLower());
+        cmd.Parameters.AddWithValue("@sr_id", hdn1.Value.ToString());
+        cmd.Parameters.AddWithValue("@item_id", ddl_product.SelectedValue.ToString());
+        cmd.Parameters.AddWithValue("@item_name", ddl_product.SelectedItem.ToString());
+        cmd.Parameters.AddWithValue("@qty", qty.Text.ToString());
+        cmd.Parameters.AddWithValue("@rate", rate.Text.ToString());
+        cmd.Parameters.AddWithValue("@unit", ddl_unit.SelectedValue.ToString());
+        cmd.Parameters.AddWithValue("@total", txt_total_cost.Text.ToString());
+        cmd.Parameters.AddWithValue("@customer_id", ddl_cs.SelectedValue.ToString());
+        cmd.Parameters.AddWithValue("@customer_name", ddl_cs.SelectedItem.ToString());
+        cmd.Parameters.AddWithValue("@narration", txt_narration.Text.ToString());
+        cmd.Parameters.Add("@result", SqlDbType.NVarChar, 500);
+        cmd.Parameters["@result"].Direction = ParameterDirection.Output;
+        cmd.Connection = con;
+        con.Open();
+        cmd.ExecuteNonQuery();
+        lbl_msg.Text = cmd.Parameters["@result"].Value.ToString();
+        BindListView1();
+        btn_addmore.Text = "Add More";
+        clear1();
+    }
+    protected void btn_final_Click(object sender, EventArgs e)
+    {
+        if (btn_final.Text == "Save")
+        {
+
+
+            string QryforTemp = "select * from dbo.tbl_temp_sell_return where customer_id='" + ddl_cs.SelectedValue + "'";
+            // DataSet dt = new DataSet();
+            SqlConnection con = new SqlConnection(conn);
+
+            SqlCommand cmd = new SqlCommand(QryforTemp, con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    SqlCommand cmd1 = new SqlCommand();
+                    cmd1.CommandType = CommandType.StoredProcedure;
+                    cmd1.CommandText = "sp_sell_return";
+                    cmd1.Parameters.AddWithValue("@action", btn_final.Text.ToLower());
+                    cmd1.Parameters.AddWithValue("@sr_id", ds.Tables[0].Rows[i]["sr_id"].ToString());
+                    cmd1.Parameters.AddWithValue("@item_id", ds.Tables[0].Rows[i]["item_id"].ToString());
+                    cmd1.Parameters.AddWithValue("@item_name", ds.Tables[0].Rows[i]["item_name"].ToString());
+                    cmd1.Parameters.AddWithValue("@qty", ds.Tables[0].Rows[i]["qty"].ToString());
+                    cmd1.Parameters.AddWithValue("@rate", ds.Tables[0].Rows[i]["rate"].ToString());
+                    cmd1.Parameters.AddWithValue("@unit", ds.Tables[0].Rows[i]["unit"].ToString());
+                    cmd1.Parameters.AddWithValue("@total", ds.Tables[0].Rows[i]["total"].ToString());
+                    cmd1.Parameters.AddWithValue("@customer_id", ds.Tables[0].Rows[i]["customer_id"].ToString());
+                    cmd1.Parameters.AddWithValue("@customer_name", ds.Tables[0].Rows[i]["customer_name"].ToString());
+                    cmd1.Parameters.AddWithValue("@narration", ds.Tables[0].Rows[i]["narration"].ToString());
+                    cmd1.Parameters.Add("@result", SqlDbType.NVarChar, 500);
+                    cmd1.Parameters["@result"].Direction = ParameterDirection.Output;
+                    cmd1.Connection = con;
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    cmd1.ExecuteNonQuery();
+                    lbl_msg.Text = cmd1.Parameters["@result"].Value.ToString();
+
+                    SqlCommand cmd2 = new SqlCommand();
+                    cmd2.CommandType = CommandType.StoredProcedure;
+                    cmd2.CommandText = "sp_temp_sell_return_delete";
+                    cmd2.Parameters.AddWithValue("@customer_id", ddl_cs.SelectedValue.ToString());
+                    cmd2.Parameters.AddWithValue("@customer_name", ddl_cs.SelectedItem.ToString());
+                    cmd2.Connection = con;
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    cmd2.ExecuteNonQuery();
+                }
+            }
+        }
+        if (btn_final.Text == "update")
+        {
+            //Update();
+        }
+    }
+    protected void ListView2_ItemCommand(object sender, ListViewCommandEventArgs e)
+    {
+        Session["Id"] = e.CommandArgument;
+        try
+        {
+            id = e.CommandArgument.ToString();
+
+            if (e.CommandName == "CmdEdit")
+            {
+                btn_addmore.Text = "update";
+                string str1 = "select * from tbl_temp_sell_return where sr_id='" + e.CommandArgument + "'";
+                SqlDataReader dr = cl.selectDR(str1);
+                if (dr.Read())
+                {
+                    hdn1.Value = dr["sr_id"].ToString();
+                    ddl_product.SelectedValue = dr["item_id"].ToString();
+                    qty.Text = dr["qty"].ToString();
+                    rate.Text = dr["rate"].ToString();
+                    ddl_unit.SelectedValue = dr["unit"].ToString();
+                    txt_total_cost.Text = dr["total"].ToString();
+                    ddl_cs.SelectedValue = dr["customer_id"].ToString();
+                    txt_narration.Text = dr["narration"].ToString();
+                }
+            }
+            if (e.CommandName == "CmdDelete")
+            {
+                Session["id"] = e.CommandArgument.ToString();
+                SqlConnection con = new SqlConnection(conn);
+                id = e.CommandArgument.ToString();
+                SqlCommand cmd = new SqlCommand("delete from tbl_temp_sell_return where sr_id=" + id, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Page.ClientScript.RegisterStartupScript(Page.GetType(), "MessageBox", "<script language='javascript'>alert('Deleted Successfully');</script>");
+                BindListView1();
+            }
+        }
+        catch { }
+    }
+    protected void rate_TextChanged(object sender, EventArgs e)
+    {
+        if (rate.Text.ToString() != null || qty.Text.ToString() != null)
+        {
+            txt_total_cost.Text = Convert.ToString(Convert.ToInt32(rate.Text) * Convert.ToInt32(qty.Text));
+        }
+        else
+        {
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "error", true);
+
+        }
+    }
+    protected void ddl_cs_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        BindListView1();
+    }
+}
